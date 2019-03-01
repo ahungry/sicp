93fb45aa767f25476ddf914080d23e289662e0be
Matthew Carter <m@ahungry.com>
2019-02-28 23:46:48 -0500

    Add some stuff

--- scratch.scm
+++ scratch.scm
@@ -21,3 +21,34 @@
       guess
       (sqrt-iter (improve guess x)
                  x)))
+
+(define (my-sqrt x)
+  (sqrt-iter 1.0 x))
+
+;; 1-2-1
+;; A linear recursive process
+;; This is a recursive procedure that is describing a linear process (TCO)
+(define (factorial n)
+  (if (= n 1)
+      1
+      (* n (factorial (- n 1)))))
+
+;; Compute with a linear iterative process
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+      product
+      (fact-iter (* counter product)
+                 (+ counter 1)
+                 max-count)))
+
+(define (lfactorial n)
+  (fact-iter 1 1 n))
+
+;; Or we could define it with block structure to hide iter
+(define (bfactorial n)
+  (define (iter product counter)
+    (if (> counter n)
+        product
+        (iter (* counter product)
+              (+ counter 1))))
+  (iter 1 1))
