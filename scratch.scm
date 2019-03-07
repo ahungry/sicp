97bd330db2062a49a53685a6c2290c71a303d5f0
Matthew Carter <m@ahungry.com>
2019-03-06 22:32:56 -0500

    That's really neat

--- scratch.scm
+++ scratch.scm
@@ -109,3 +109,13 @@
   (display "/")
   (display (denom x))
   (newline))
+
+;; 2-1-3 - WOW! Defining a data representation as a procedure, neat!
+(define (my-cons x y)
+  (lambda (m) (m x y)))
+
+(define (my-car z)
+  (z (lambda (p q) p)))
+
+(define (my-cdr z)
+  (z (lambda (p q) q)))
