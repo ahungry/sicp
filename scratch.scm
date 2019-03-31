cf438d5e1fb331e65b5efb0b623f826620a1fae5
Matthew Carter <m@ahungry.com>
2019-03-30 23:08:28 -0400

    Update spo

--- scratch.scm
+++ scratch.scm
@@ -192,3 +192,27 @@
                (union-set (cdr set1) set2)))))
 
 ;; 3 Modularity
+(define balance 100)
+
+(define (withdraw amount)
+  (if (>= balance amount)
+      (begin (set! balance (- balance amount))
+             balance)
+      "Insufficient funds"))
+
+(define new-withdraw
+  (let ((balance 100))
+    (lambda (amount)
+      (if (>= balance amount)
+          (begin (set! balance (- balance amount))
+                 balance)
+          "Insufficient funds"))))
+
+(define (make-withdraw balance)
+  (lambda (amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds")))
+
+;; 3.2
