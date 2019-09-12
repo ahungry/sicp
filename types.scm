807c0dce2897d2ae4ea14b9c19dd4385ad92273d
Matthew Carter <m@ahungry.com>
2019-09-12 00:11:29 -0400

    more fun

--- types.scm
+++ types.scm
@@ -25,14 +25,27 @@
   (if (equal? (in-type f) (out-type g))
       #t
       (error "Incompatible types composed.")))
-(define (_comp f g) (lambda (x) (invoke f (invoke g x))))
+(define (_comp f g)
+  (fn (out-type f) (in-type g)
+      (lambda (x) (invoke f (invoke g x)))))
 (define (comp f g)
   (if (equal? 'type-check mode)
       (begin (assert-types f g) (_comp f g))
       (_comp f g)))
 
+(define (_range count iter ns)
+  (if (>= (length ns) count)
+      ns
+      (_range count (+ 1 iter) (cons iter ns))))
+(define range (fn 'number 'numbers (lambda (n) (reverse (_range n 0 #nil)))))
+
 ;; Will compose just fine.
 (define add-one->string (comp stringer add-one))
+(define add-2 (comp add-one add-one))
+(invoke add-2 3)                        ; produces 5
+
+(define add-4 (comp add-2 add-2))
+(invoke add-4 10)                       ; produces 14
 
 ;;  Will trigger an error.
 (define add-one-to-string (comp add-one stringer))
